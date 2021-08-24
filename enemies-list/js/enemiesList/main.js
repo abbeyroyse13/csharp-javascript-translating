343380c50a39f1c66ea92f8f5bf6198f3ff86f7e
abbeyroyse13 <heatherruan126@gmail.com>
2021-08-24 14:43:42 -0500

    enemies done

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,5 +1,57 @@
 // Put your code here
 
-// for this I need to make a list of enemies 
-// I then need to make booleans to show the true/false 
-// then the enemies need to be returned 
+console.log("My Enemies List!");
+console.log("----------------");
+
+const enemyList = () => {
+
+    const enemies = [
+        {
+            FirstName: "Billy",
+            LastName: "Loomis",
+            Offenses: [
+                "Killed like 4 people",
+                "Was mean to his gf"
+            ],
+            IsReallyHated: false
+        },
+        {
+            FirstName: "Stu",
+            LastName: "Macher",
+            Offenses: [
+                "Also killed like 4 people",
+                "Annoying in a baffling way"
+            ],
+            IsReallyHated: false
+        },
+        {
+            FirstName: "Roman",
+            LastName: "Bridger",
+            Offenses: [
+                "He's whiny about everything",
+                "He hates his half sis for reasons she can't control"
+            ],
+            IsReallyHated: true
+        },
+        {
+            FirstName: "Mickey",
+            LastName: "Altieri",
+            Offenses: [
+                "Has poor taste in movies",
+                "Doesn't like Randy Meeks"
+            ],
+            IsReallyHated: true
+        }
+    ];
+    return enemies;
+}
+
+const enemies = enemyList();
+
+for (const enemy of enemies) {
+    if (enemy.IsReallyHated) {
+        console.log(`${enemy.FirstName} ${enemy.LastName} (Really really dislike!)`)
+    } else {
+        console.log(`${enemy.FirstName} ${enemy.LastName}`)
+    }
+};
