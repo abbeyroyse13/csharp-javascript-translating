9e28642061884b485d29c2ca3e80b4a705abb263
abbeyroyse13 <heatherruan126@gmail.com>
2021-08-26 14:38:56 -0500

    die roll done

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -5,12 +5,19 @@ console.log("---------------------------");
 
 // use this somewhere 
 
-// function getRandomInt(min, max) {
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
+const Roll = () => {
+    min = Math.ceil(1);
+    max = Math.floor(6);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-const dieToString = () => {
+for (let i = 0; i < 10; i++) {
+    let die1 = Roll();
+    let die2 = Roll();
 
+    let message = `${die1} + ${die2} = ${die1 + die2}`
+    if (die1 === die2) {
+        message += "DOUBLES!";
+    }
+    console.log(message)
 }
